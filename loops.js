5d0fc4174159d88505ea09ba1365532bd179bf04
Patrice Drayton <draytonpatrice@marilynssecret.org>
2018-09-14 18:41:06 +0000

    automatically backed up by learn

--- loops.js
+++ loops.js
@@ -19,5 +19,16 @@ function forLoop(array){
    return ('done');
  }    
 }
+function doWhileLoop(array){
+function maybeTrue() {
+  return Math.random() >= 0.5;
+}
+do{
+  array.pop();
+}
+while (array.length > 0 && maybeTrue())
+return array ;
+}
 
+doWhileLoop([1,2,3,4,5])
    
